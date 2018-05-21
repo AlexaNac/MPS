fb1a1043a7dd6e18084ebf02a35c0195f96aecc8
Diana Dumitru <dianadmaria95@gmail.com>
2018-05-21 18:26:06 +0300

    - created database, context entities as well as table constraints

---
+++ CRM-dentalClinic/Storage/PacientEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM_dentalClinic.Storage
+{
+    public class PacientEntity
+    {
+    }
+}
